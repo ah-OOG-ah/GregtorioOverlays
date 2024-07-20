8c6b113f330070541a4016219608574278e560de
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-07-20 18:43:01 -0400

    Fix button i18n
    Also make sure GT is no longer hard depped

--- src/main/java/klaxon/klaxon/goverlays/Backend.java
+++ src/main/java/klaxon/klaxon/goverlays/Backend.java
@@ -1,5 +1,10 @@
 package klaxon.klaxon.goverlays;
 
+/**
+ * As of right now, it's not possible to run GT5NH and HBM together, and GT6 doesn't have pollution, so there's no point
+ * supporting multiple backends in parallel. My hope is that if GT6 adds pollution it'll cooperate with NTM so I don't
+ * have to make another one...
+ */
 public enum Backend {
     GT5U,
     HBM_NTM, // shhhh
