20fb32f8b3dd70bd170c5141cb4e3245ce1384b5
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-11-06 17:34:40 -0500

    Refactor PollutionMessage

--- src/main/java/klaxon/klaxon/goverlays/network/pollution/PollutionMessage.java
+++ src/main/java/klaxon/klaxon/goverlays/network/pollution/PollutionMessage.java
@@ -19,8 +19,8 @@
 package klaxon.klaxon.goverlays.network.pollution;
 
 import static klaxon.klaxon.goverlays.GregtorioOverlays.LOGGER;
-
-import java.util.ConcurrentModificationException;
+import static klaxon.klaxon.goverlays.network.pollution.PollutionMessage.Error.BUFFER_OVERFLOW;
+import static klaxon.klaxon.goverlays.network.pollution.PollutionMessage.Error.fromInt;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -30,17 +30,13 @@ import it.unimi.dsi.fastutil.longs.Long2IntMaps;
 import it.unimi.dsi.fastutil.longs.Long2IntOpenHashMap;
 
 /**
- * Sent from the server, giving clients a list of pollution chunks
- * This will send as many packets as needed to send the data.
+ * Sent from the server, giving clients a list of pollution chunks.
  */
 public class PollutionMessage implements IMessage {
 
     // Store a set of pollution chunks
-    private Long2IntOpenHashMap chunks;
-    public int dimension;
-
-    // Flags
-    private final boolean concurrentException;
+    private final Long2IntOpenHashMap chunks;
+    public int dimID;
 
     @NotNull
     public Long2IntOpenHashMap getChunks() {
@@ -48,46 +44,36 @@ public class PollutionMessage implements IMessage {
     }
 
     // Constructor from nothing, because it's needed for registration or smth idk
+    @SuppressWarnings("unused")
     public PollutionMessage() {
-        this(0, null, false);
+        this(0, new Long2IntOpenHashMap());
     }
 
-    public PollutionMessage(ConcurrentModificationException e) {
-        this(0, null, true);
-    }
-
-    // Constructors from data
-    public PollutionMessage(int dimID, Long2IntOpenHashMap sendChunks, boolean isExcept) {
-        chunks = (sendChunks == null) ? new Long2IntOpenHashMap() : sendChunks;
-        dimension = dimID;
-        concurrentException = isExcept;
-    }
-
-    public PollutionMessage addAll(Long2IntOpenHashMap pollutionMap) {
-        chunks.putAll(pollutionMap);
-        return this;
+    public PollutionMessage(int dimID, @NotNull Long2IntOpenHashMap chunks) {
+        this.chunks = chunks;
+        this.dimID = dimID;
     }
 
     @Override
     public void fromBytes(ByteBuf buf) {
-
         // Read the header
-        int numChunks = buf.readInt();
+        final int numChunks = buf.readInt();
+        final Error err = fromInt(numChunks);
 
-        if (numChunks == -2) {
-
-            LOGGER.warn("Server had a ConcurrentModificationException!");
-            LOGGER.warn("Updating client pollution chunks failed!");
-            return;
-        } else if (numChunks == -1) {
-
-            LOGGER.warn("Server has too many polluted chunks to send a PollutionMessage!");
-            LOGGER.warn("Updating client pollution chunks failed!");
-            return;
-        } else if (numChunks == 0) {
-
-            LOGGER.debug("No polluted chunks need updating, skipping rest of packet");
-            return;
+        switch (err) {
+            case NONE -> {
+                LOGGER.debug("No polluted chunks need updating, skipping rest of packet");
+                return;
+            }
+            case BUFFER_OVERFLOW -> {
+                LOGGER.warn("Server has too many polluted chunks to send a PollutionMessage!");
+                LOGGER.warn("Updating client pollution chunks failed!");
+                return;
+            }
+            case UNKNOWN -> {
+                LOGGER.warn("Invalid packet received! The server encountered an unknown error when sending chunks.");
+                return;
+            }
         }
 
         // There are chunks, validate message. Should be 12 * number of chunks + 4, as header has already been read
@@ -100,65 +86,78 @@ public class PollutionMessage implements IMessage {
         }
 
         // Message valid, read and add chunks
-        dimension = buf.readInt();
+        dimID = buf.readInt();
+        chunks.ensureCapacity(numChunks);
         for (int i = 0; i < numChunks; i++) {
             chunks.put(buf.readLong(), buf.readInt());
         }
-
-        // done!
     }
 
     /**
      * This converts the HashSet of PollutionChunkPositions to a stream of bytes. It must:
+     * <ul>
      * <li>Add a header showing how many chunks are in the message
      * <li>Validate this, making sure that enough space is available for the message
      * <li>Write each position as raw bytes, with no separator; this can be done because each chunk is the same size.
+     * </ul>
      * While this is slightly less efficient, it's easier, plus Netty compresses packets anyways.
      *
      * @param buf Buffer to write to
      */
     @Override
     public void toBytes(ByteBuf buf) {
+        // Validate and write the header
+        // There are four ints in a PCP, each of which take 4 bytes; 16 bytes per PCP
+        // Add the space taken by the header and dimension and log if there's an error
+        int num = chunks.size();
+        int msgSize = 8 + num * 12;
+        try {
+            buf.ensureWritable(msgSize);
+        } catch (IndexOutOfBoundsException e) {
 
-        // Check for errors
-        if (concurrentException) {
+            // Log and send a -1 to let the client know
+            LOGGER.error(e.toString());
+            LOGGER.error(
+                "Too many chunks to send in one packet! Trying to send " + num
+                    + " chunks through a "
+                    + buf.maxCapacity()
+                    + "B buffer.");
+            buf.writeInt(BUFFER_OVERFLOW.value);
+            return;
+        }
 
-            // Write the header
-            buf.writeInt(-2);
-        } else {
+        // Write the header + dim
+        buf.writeInt(num);
+        buf.writeInt(dimID);
 
-            // No errors yet, proceed
-            // Count the chunks
-            int num = chunks.size();
+        // Iterate over HashSet
+        Long2IntMaps.fastForEach(chunks, le -> {
+            buf.writeLong(le.getLongKey());
+            buf.writeInt(le.getIntValue());
+        });
+    }
 
-            // Validate and write the header
-            // There are four ints in a PCP, each of which take 4 bytes; 16 bytes per PCP
-            // Add the space taken by the header and dimension and log if there's an error
-            int msgSize = 8 + num * 12;
-            try {
-                buf.ensureWritable(msgSize);
-            } catch (IndexOutOfBoundsException e) {
+    /**
+     * Errors are all negative, as negative numbers of chunks to update do not make sense.
+     */
+    protected enum Error {
 
-                // Log and send a -1 to let the client know
-                LOGGER.error(e.toString());
-                LOGGER.error(
-                    "Too many chunks to send in one packet! Trying to send " + num
-                        + " chunks through a "
-                        + buf.maxCapacity()
-                        + "B buffer.");
-                buf.writeInt(-1);
-                return;
-            }
+        NONE(0),
+        BUFFER_OVERFLOW(-1),
+        UNKNOWN(Integer.MIN_VALUE);
 
-            // Write the header + dim
-            buf.writeInt(num);
-            buf.writeInt(dimension);
+        final int value;
 
-            // Iterate over HashSet
-            Long2IntMaps.fastForEach(chunks, le -> {
-                buf.writeLong(le.getLongKey());
-                buf.writeInt(le.getIntValue());
-            });
+        Error(int value) {
+            this.value = value;
+        }
+
+        static Error fromInt(int i) {
+            if (i >= 0) return NONE;
+            return switch (i) {
+                case -1 -> BUFFER_OVERFLOW;
+                default -> UNKNOWN;
+            };
         }
     }
 }
